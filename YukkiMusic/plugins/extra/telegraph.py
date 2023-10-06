2550b4da9996018a3a1918321091f11cacd1962d
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-06 18:10:19 +0300

    Update and rename extra.py to telegraph.py

--- YukkiMusic/plugins/extra/telegraph.py
+++ YukkiMusic/plugins/extra/telegraph.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message
 from telegraph import upload_file
 import os
 from strings.filters import command
-from AnonX import app
+from YukkiMusic import app
 
 @app.on_message(command(["تێلەگراف","telegraph"]))
 async def get_link_group(client, message):
