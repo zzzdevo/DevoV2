85d065b2948475ba61d52c23d0548b6f443689fd
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-06 18:17:20 +0300

    Update zxrfa.py

--- YukkiMusic/plugins/extra/zxrfa.py
+++ YukkiMusic/plugins/extra/zxrfa.py
@@ -5,7 +5,7 @@ from config import MUSIC_BOT_NAME
 from strings.filters import command
 from pyrogram import Client
 from pyrogram.types import Message
-from AnonX import app
+from YukkiMusic import app
 
 @app.on_message(command("ز"))
 async def zahrafa(c: Client, m: Message):
