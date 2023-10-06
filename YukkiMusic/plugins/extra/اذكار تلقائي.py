4867f673126cb9e08799fb4e16f1253bc1bf5e00
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-06 18:15:11 +0300

    Update اذكار تلقائي.py

--- YukkiMusic/plugins/extra/اذكار تلقائي.py
+++ YukkiMusic/plugins/extra/اذكار تلقائي.py
@@ -2,7 +2,7 @@ import time
 import asyncio
 from config import OWNER_ID
 from pyrogram import Client, filters
-from AnonX import app
+from YukkiMusic import app
 import random
 from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
