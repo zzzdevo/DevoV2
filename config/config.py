5cc21f8f65c4d2c1413afb2563696ee0cfc34da2
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-06 21:50:19 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -69,6 +69,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MGIMT")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/IQSUPP")
 
+SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
 
