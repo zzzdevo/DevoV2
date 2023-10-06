376b2c6266214670908916eabc9e953cb133d527
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-06 20:30:34 +0300

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -1,21 +1,14 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
+
 #
 
-from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
+from config import LOG_GROUP_ID, MUSIC_BOT_NAME
 from YukkiMusic import app
 from YukkiMusic.utils.database import is_on_off
 
 
 async def play_logs(message, streamtype):
-    if await is_on_off(LOG):
-         logger_text = f"""
+    if await is_on_off(2):
+        logger_text = f"""
 <b>{MUSIC_BOT_NAME} ئاماری پەخشکردن</b>
 
 <b>ئایدی گرووپ :</b> <code>{message.chat.id}</code>
@@ -31,8 +24,8 @@ async def play_logs(message, streamtype):
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
-                    LOG_GROUP_ID,
-                    f"{logger_text}",
+                    chat_id=LOG_GROUP_ID,
+                    text=logger_text,
                     disable_web_page_preview=True,
                 )
             except:
