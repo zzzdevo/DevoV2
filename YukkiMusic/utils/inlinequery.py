a31ad63e4ed2b224cc22f1324b1ac231446c509d
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-07 09:11:06 +0300

    Update inlinequery.py

--- YukkiMusic/utils/inlinequery.py
+++ YukkiMusic/utils/inlinequery.py
@@ -1,13 +1,3 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from pyrogram.types import (InlineQueryResultArticle,
                             InputTextMessageContent)
 
@@ -16,57 +6,39 @@ answer = []
 answer.extend(
     [
         InlineQueryResultArticle(
-            title="Pause Stream",
-            description=f"Pause the current playout on group call.",
-            thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
+            title="✵ْ وەستانی کاتی ✵ْ",
+            description=f"•⎆┊ وەستاندنی پەخشکردن بۆ ماوەیەکی کاتی♥•",
+            thumb_url="https://telegra.ph/file/224b2f3de8d7f12bd4fec.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
-            title="Resume Stream",
-            description=f"Resume the ongoing playout on group call.",
-            thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
+            title="✵ْ دەستپێکردنەوە ✵ْ",
+            description=f"•⎆┊ دەستپێکردنەوەی پەخشکردن کاتێك وەستێنراوە♥•",
+            thumb_url="https://telegra.ph/file/224b2f3de8d7f12bd4fec.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
-            title="Mute Stream",
-            description=f"Mute the ongoing playout on group call.",
-            thumb_url="https://telegra.ph/file/66516f2976cb6d87e20f9.png",
-            input_message_content=InputTextMessageContent("/mute"),
-        ),
-        InlineQueryResultArticle(
-            title="Unmute Stream",
-            description=f"Unmute the ongoing playout on group call.",
-            thumb_url="https://telegra.ph/file/3078794f9341ffd582e18.png",
-            input_message_content=InputTextMessageContent("/unmute"),
-        ),
-        InlineQueryResultArticle(
-            title="Skip Stream",
-            description=f"Skip to next track. | For Specific track number: /skip [number] ",
-            thumb_url="https://telegra.ph/file/98b88e52bc625903c7a2f.png",
+            title="✵ْ تێپەڕاندن ✵ْ",
+            description=f"•⎆┊ تێپەڕاندنی گۆرانی یان ڤیدیۆی پەخشکراو بۆ پەخشێکی تر♥•",
+            thumb_url="https://telegra.ph/file/224b2f3de8d7f12bd4fec.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
-            title="End Stream",
-            description="Stop the ongoing playout on group call.",
-            thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
-            input_message_content=InputTextMessageContent("/stop"),
+            title="✵ْ وەستان ✵ْ",
+            description="•⎆┊ کۆتایی هێنان بە پەخشکردن♥•",
+            thumb_url="https://telegra.ph/file/224b2f3de8d7f12bd4fec.jpg",
+            input_message_content=InputTextMessageContent("/end"),
         ),
         InlineQueryResultArticle(
-            title="Shuffle Stream",
-            description="Shuffle the queued tracks list.",
-            thumb_url="https://telegra.ph/file/7f6aac5c6e27d41a4a269.png",
+            title="✵ْ تێکەڵکردن ✵ْ",
+            description="•⎆┊ تێکەڵکردنی گۆرانییە ڕیزبەندەکان لە لیستی پەخشکردن♥•",
+            thumb_url="https://telegra.ph/file/224b2f3de8d7f12bd4fec.jpg",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
-            title="Seek Stream",
-            description="Seek the ongoing stream to a specific duration.",
-            thumb_url="https://telegra.ph/file/cd25ec6f046aa8003cfee.png",
-            input_message_content=InputTextMessageContent("/seek 10"),
-        ),
-        InlineQueryResultArticle(
-            title="Loop Stream",
-            description="Loop the current playing music. | Usage: /loop [enable|disable]",
-            thumb_url="https://telegra.ph/file/081c20ce2074ea3e9b952.png",
+            title="✵ْ دووبارەکردنەوە ✵ْ",
+            description="•⎆┊ دووبارە کردنەوەی گۆرانی♥•",
+            thumb_url="https://telegra.ph/file/224b2f3de8d7f12bd4fec.jpg",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
     ]
