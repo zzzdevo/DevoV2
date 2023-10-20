be5153f3cc5293f179b529d763a6d9531d4b2a9b
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-20 17:10:59 +0300

    Update call.py

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -4,7 +4,6 @@
 # This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
 # All rights reserved.
 #
 
