52344a73dcbed744feece049bc1677156980ae6d
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-20 17:18:38 +0300

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -1,16 +1,7 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
-from config import LOG, LOG_GROUP_ID
+from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
 from YukkiMusic import app
 from YukkiMusic.utils.database import is_on_off
-
+from pyrogram.types import Message
 
 async def play_logs(message, streamtype):
     if await is_on_off(LOG):
@@ -19,22 +10,21 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "گرووپی تایبەت"
         logger_text = f"""
-**ئاماری پەخشکردن: [𝙄𝙌 𝙈𝙐𝙎𝙄𝘾 ♥️•](t.me/mgimt)**
+**{MUSIC_BOT_NAME} پەخشی گۆرانی**
 
-**ناوی گرووپ:** {message.chat.title} [`{message.chat.id}`]
-**ناوی:** {message.from_user.mention}
-**یوزەری:** @{message.from_user.username}
-**ئایدی بەکار‌هێنەر:** `{message.from_user.id}`
-**یوزەری گرووپ:** {chatusername}
+**گرووپ :** {message.chat.title} [`{message.chat.id}`]
+**یوزەر :** @{message.from_user.username}
+**ئایدی :** `{message.from_user.id}`
+**بەستەری گرووپ :** {chatusername}
 
-**ڕیزکراو:** {message.text}
+**گەڕان بۆ :** {message.text}
 
-**جۆری پەخشکردن:** {streamtype}"""
+**جۆری پەخش :** {streamtype}"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
                     LOG_GROUP_ID,
-                    f"{logger_text}",
+                    text=logger_text,
                     disable_web_page_preview=True,
                 )
             except:
